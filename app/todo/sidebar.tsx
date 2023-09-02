0017c636feea8718a17292513068903f8a393df4
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-09-02 17:15:34 +0800

    [todo/sidebar] Set sidebar menu as a component

--- app/todo/sidebar.tsx
+++ app/todo/sidebar.tsx
@@ -1,4 +1,31 @@
+import Link from "next/link"
+
+function SidebarMenuContainer({name, url}: {name: string, url: string}){
+    return (
+        <Link href={url}>
+            <button type="button">{name}</button>
+        </Link>
+    )
+}
+
 export default function Sidebar(){
+    const sidebar_menu_list = [
+        {
+            'id': 0,
+            'name': 'Home',
+            'url': '#'
+        },
+        {
+            'id': 1,
+            'name': 'Login',
+            'url': '#'
+        },
+        {
+            'id': 2,
+            'name': 'Logout',
+            'url': '#'
+        }
+    ]
     return (
         <div className="p-2">
             <div className="bg-yellow-200 flex flex-1">
@@ -6,17 +33,15 @@ export default function Sidebar(){
             </div>
             {/*  sidebar menu */}
             <div className="flex flex-col">
-                <button type="button">
-                    Home
-                </button>
-                
-                <button type="button">
-                    Login
-                </button>
-
-                <button type="button">
-                    Log-out
-                </button>
+                {sidebar_menu_list.map(
+                    data => (
+                        <SidebarMenuContainer
+                            name={data.name}
+                            url={data.url}
+                            key={data.id}
+                        />
+                    )    
+                )}
             </div>
         </div>
     )
