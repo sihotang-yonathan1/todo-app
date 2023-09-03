18655cdf828d80558d6fe5c5a11f0c932b942867
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-09-03 12:03:11 +0800

    [todo/sidebar] reconfigure sidebar ui

--- app/todo/sidebar.tsx
+++ app/todo/sidebar.tsx
@@ -2,9 +2,12 @@ import Link from "next/link"
 
 function SidebarMenuContainer({name, url}: {name: string, url: string}){
     return (
-        <Link href={url}>
-            <button type="button">{name}</button>
-        </Link>
+        <div className="hover:bg-[#84DBD6] hover:text-slate-200 py-1 text-left border-2 my-1 mx-1
+        px-1">
+            <Link href={url}>
+                <button type="button">{name}</button>
+            </Link>
+        </div>
     )
 }
 
@@ -17,22 +20,17 @@ export default function Sidebar(){
         },
         {
             'id': 1,
-            'name': 'Login',
-            'url': '#'
-        },
-        {
-            'id': 2,
             'name': 'Logout',
-            'url': '#'
+            'url': '/login'
         }
     ]
     return (
-        <div className="p-2 bg-[#84b5db]">
-            <div className="bg-yellow-200 flex flex-1">
+        <div className="bg-[#84b5db]">
+            <div className="bg-yellow-200 flex flex-1 py-2 px-2">
                 <p>User data</p>
             </div>
             {/*  sidebar menu */}
-            <div className="flex flex-col">
+            <div className="flex flex-col py-2">
                 {sidebar_menu_list.map(
                     data => (
                         <SidebarMenuContainer
