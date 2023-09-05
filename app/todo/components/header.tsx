639bf1e6940720971278e62bd45f40f89786098c
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-09-05 13:17:05 +0800

    [todo/component] move header and sidebar to `/todo/components`

---
+++ app/todo/components/header.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { FiMenu, FiUser } from "react-icons/fi"
+
+export function Header({isSidebarVisible, setSidebarVisible}: {isSidebarVisible: boolean, setSidebarVisible: React.Dispatch<React.SetStateAction<boolean>>}){
+    return (
+        // Header
+        <div className="flex flex-row px-2 py-1 items-center">
+            {/* Left */}
+            <div className="flex flex-row">
+                {/* TODO: open sidebar if clicked */}
+                <button 
+                    type="button"
+                    onClick={() => setSidebarVisible(!isSidebarVisible)}
+                >
+                    <FiMenu />
+                </button>
+            </div>
+
+            {/* Right */}
+            <div className="ml-auto">
+                <button className=" bg-slate-500 rounded-full p-1">
+                    {/* TODO: change icon to user avatar if available */}
+                    <FiUser color="#efefef"/>
+                </button>
+            </div>
+
+        </div>
+    )
+}
--- app/todo/layout.tsx
+++ app/todo/layout.tsx
@@ -1,9 +1,9 @@
 "use client"
+
 import React, { useState } from "react";
 
-import { FiMenu, FiUser } from "react-icons/fi";
-
-import Sidebar from "./sidebar";
+import Sidebar from "./components/sidebar";
+import { Header } from "./components/header";
 
 export default function TodoAppLayout({children}: {children: React.ReactNode}){
     const [isSidebarVisible, setSidebarVisible] = useState(false)
@@ -13,29 +13,7 @@ export default function TodoAppLayout({children}: {children: React.ReactNode}){
             {/*  sidebar */}
             { isSidebarVisible && <Sidebar />}
             <div className="flex flex-col flex-grow bg-[#EE4E34]">
-                {/* Header */}
-                <div className="flex flex-row px-2 py-1 items-center">
-
-                    {/* Left */}
-                    <div className="flex flex-row">
-                        {/* TODO: open sidebar if clicked */}
-                        <button 
-                            type="button"
-                            onClick={() => setSidebarVisible(!isSidebarVisible)}
-                        >
-                            <FiMenu />
-                        </button>
-                    </div>
-
-                    {/* Right */}
-                    <div className="ml-auto">
-                        <button className=" bg-slate-500 rounded-full p-1">
-                            {/* TODO: change icon to user avatar if available */}
-                            <FiUser color="#efefef"/>
-                        </button>
-                    </div>
-
-                </div>
+                <Header isSidebarVisible={isSidebarVisible} setSidebarVisible={setSidebarVisible}/>
                 {/* Content Container */} 
                 <div className="bg-[#e79183] min-h-screen flex flex-col items-center justify-center">
                     {children}
