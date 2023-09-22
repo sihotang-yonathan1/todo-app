2740e131718706b8e53f189721f1c1a750b459b7
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-09-22 10:06:29 +0800

    [todo/todo_page] Pad time value to 2 number digits

--- app/todo/todo_page.tsx
+++ app/todo/todo_page.tsx
@@ -50,7 +50,11 @@ function TaskContainer({name, id, created_time, deleteFunct}: {name: string, id:
                         onChange={handleTaskNameEdit}
                     />
                     <div>
-                        <p className="text-xs italic">Created at {created_time.getHours().toString()}:{created_time.getMinutes().toString()}:{created_time.getSeconds()}</p>
+                        <p className="text-xs italic">
+                            Created at 
+                            {created_time.getHours().toString().padStart(2, "0")}
+                            :{created_time.getMinutes().toString().padStart(2, "0")}
+                            :{created_time.getSeconds().toString().padStart(2, "0")}</p>
                     </div>
                 </div>
 
