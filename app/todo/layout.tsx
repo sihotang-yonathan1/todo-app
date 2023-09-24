764544572393ba182ddadfc494af0666c054d49c
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-09-24 16:29:23 +0800

    [todo] redirect to '/login' if not login

--- app/todo/layout.tsx
+++ app/todo/layout.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Header } from "./components/header";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default function TodoAppLayout({children}: {children: React.ReactNode}){
+    const cookie_list = cookies()
+
+    if (!cookie_list.has('user_id')){
+        redirect('/login')
+    }
     return (
         <div className="flex flex-row">
             <div className="flex flex-col flex-grow bg-[#EE4E34]">
