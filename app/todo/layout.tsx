6bdc8b9a238f8db101a3ca6b37ebf42297dfee9f
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-09-29 15:26:03 +0800

    [todo/layout] Add sidebar again
    This sidebar still can't be hide by hamburger button

--- app/todo/layout.tsx
+++ app/todo/layout.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Header } from "./components/header";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import SidebarPage from "./components/sidebar";
 
 export default function TodoAppLayout({children}: {children: React.ReactNode}){
     return (
         <div className="flex flex-row">
+            <SidebarPage />
             <div className="flex flex-col flex-grow bg-[#EE4E34]">
                 <Header/>
                 {/* Content Container */} 
