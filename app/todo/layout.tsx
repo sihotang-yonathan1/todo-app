7b14e78a0636058e4c1e12aa6aa0010e9161e1dc
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-10-01 18:12:15 +0800

    [todo/layout] Fix layout overflow

--- app/todo/layout.tsx
+++ app/todo/layout.tsx
@@ -6,12 +6,12 @@ import SidebarPage from "./components/sidebar";
 
 export default function TodoAppLayout({children}: {children: React.ReactNode}){
     return (
-        <div className="flex flex-row">
+        <div className="flex flex-row h-screen">
             <SidebarPage />
             <div className="flex flex-col flex-grow bg-[#EE4E34]">
                 <Header/>
                 {/* Content Container */} 
-                <div className="bg-[#e79183] h-screen flex flex-col items-center justify-center max-h-full">
+                <div className="bg-[#e79183] flex flex-col items-center justify-center flex-1">
                     {children}
                 </div>
             </div>
