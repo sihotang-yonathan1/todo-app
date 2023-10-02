098078f9cd3c74c52271e2a20cdbaf8157218800
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-10-02 18:32:35 +0800

    [todo/component/LowerSidebar] Set logout functionality

--- app/todo/components/sidebar/LowerSidebar.tsx
+++ app/todo/components/sidebar/LowerSidebar.tsx
@@ -1,7 +1,26 @@
+"use client"
+
+import * as dotenv from 'dotenv'
+import { useRouter } from 'next/navigation'
+// This code using default xampp config that may not the same as other
+// please change with your own configuration
+dotenv.config()
+
 export default function LowerSidebar(){
+    const router = useRouter()
     return (
         <div>
-            <p>Lower sidebar here</p>
+            <button type="button" onClick={e => {
+                const logOutFunction = async () => {
+                    const response = await fetch(`http://localhost:3000/api/v1/logout`, {
+                        method: "POST"
+                    })
+                    if (response.ok){
+                        router.replace("/login")
+                    }
+                }
+                logOutFunction()
+            }}>Log-out</button>
         </div>
     )
 }
